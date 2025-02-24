4f527e9ad443cfc786d4f8b84d4c80e9faf3ab21
Nikita-mnd <nikita.morozov.mnd@gmail.com>
2025-02-24 17:31:39 +0300

    uppppp

---
+++ task00/logic.cpp
@@ -0,0 +1,21 @@
+#include "main.h" 
+
+int time(int play) {
+	int time;
+
+	return time;
+}
+
+
+
+
+
+string creat_random_exampels(int play) {
+
+	string examples = "";
+
+	examples = to_string(random_number()) + " " + to_string(random_number())+
+		" "+ to_string(random_number()) + "=";
+
+	return examples;
+}
---
+++ task00/util.cpp
@@ -0,0 +1,18 @@
+#include "main.h"
+
+int random_number() {
+
+	int number = rand() % 10 + 1;
+	return number;
+}
+
+int random_sign() {
+	int sign;
+	if (random_number() % 2 == 0) {
+		sign = -1;
+	}
+	else {
+		sign = 1;
+	}
+	return sign;
+}
